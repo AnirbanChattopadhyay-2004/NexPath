e9771d7a0f89e2a83af85c617983ddcd0d7e2777
AnirbanChattopadhyay-2004 <anirban.chtt@gmail.com>
2024-09-12 15:02:39 +0530

    Hackathon project

---
+++ src/app/signin/[[...signin]]/page.tsx
@@ -0,0 +1,13 @@
+import { BackgroundBeamsWithCollision } from '@/components/ui/background-beams-with-collision'
+import { SignIn } from '@clerk/nextjs'
+
+const Signin = () => {
+  return (
+  <BackgroundBeamsWithCollision>
+     <SignIn />
+  </BackgroundBeamsWithCollision>
+
+  )
+}
+
+export default Signin
---
+++ src/app/signup/[[...signup]]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SignUp } from '@clerk/nextjs'
+import { BackgroundBeamsWithCollision } from '@/components/ui/background-beams-with-collision'
+const Signup = () => {
+  return (
+  <BackgroundBeamsWithCollision>
+    <SignUp />
+ </BackgroundBeamsWithCollision>
+
+  )
+}
+
+export default Signup
